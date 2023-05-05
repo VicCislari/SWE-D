f2d8409ca656070ea6debb7763c4bbf0d1dc7015
VicCislari <victorion1402@gmail.com>
2023-05-05 20:29:47 +0200

    reservation basic struct

--- Reservation.java
+++ Reservation.java
@@ -1,3 +1,41 @@
-public class Reservation {
-    
-}
+import java.time.LocalDateTime;
+
+class Reservation{
+    private LocalDateTime awaited_pick_up;
+    private Lender lender;
+    private Rental rental;
+    private Title title;
+
+    public LocalDateTime getAwaited_pick_up() {
+        return this.awaited_pick_up;
+    }
+
+    public void setAwaited_pick_up(LocalDateTime awaited_pick_up) {
+        this.awaited_pick_up = awaited_pick_up;
+    }
+
+    public Lender getLender() {
+        return this.lender;
+    }
+
+    public void setLender(Lender lender) {
+        this.lender = lender;
+    }
+
+    public Rental getRental() {
+        return this.rental;
+    }
+
+    public void setRental(Rental rental) {
+        this.rental = rental;
+    }
+
+    public Title getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(Title title) {
+        this.title = title;
+    }
+
+  }
