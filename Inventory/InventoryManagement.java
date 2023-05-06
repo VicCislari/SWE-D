368247a24e6f05a54ea46cebd45b05613d50ec62
earlyo <earlyo@mailbox.org>
2023-05-06 13:55:14 +0200

    a

--- Inventory/InventoryManagement.java
+++ Inventory/InventoryManagement.java
@@ -1,5 +1,29 @@
+//importing the packages
+import Inventory.*;
+import Title.*;
+import Lender.*;
+import Reservation.*;
+
 package Inventory;
 
 public class InventoryManagement {
     
+
+    private Copy[] copies;
+    
+    public void setCopy() {
+        this.copies = new Copy[0];
+    }
+
+
+
+    public void createCopy(Title title){
+        Copy copy = new Copy(null, null, false, title);
+        copies.add();
+    }
+
+
+
+
+
 }
