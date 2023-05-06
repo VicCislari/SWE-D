57c55a956be13188b052d17c1c84668c1650b343
VicCislari <victorion1402@gmail.com>
2023-05-06 14:05:43 +0200

    inventoryManagement

--- Inventory/InventoryManagement.java
+++ Inventory/InventoryManagement.java
@@ -1,25 +1,35 @@
 //importing the packages
+package Inventory;
+
 import Inventory.*;
 import Title.*;
 import Lender.*;
 import Reservation.*;
 
-package Inventory;
-
 public class InventoryManagement {
     
-
     private Copy[] copies;
-    
-    public void setCopy() {
-        this.copies = new Copy[0];
+
+    public Copy[] getCopies() {
+        return this.copies;
     }
 
-
+    public void setCopies(Copy[] copies) {
+        this.copies = copies;
+    }
 
     public void createCopy(Title title){
-        Copy copy = new Copy(null, null, false, title);
-        copies.add();
+        Copy copy = new Copy("1", "1", false, title);
+        addCopy(copy);
+    }
+    
+    private void addCopy(Copy newCopy) {
+        Copy[] newCopies = new Copy[copies.length + 1];
+        for (int i = 0; i < copies.length; i++) {
+            newCopies[i] = copies[i];
+        }
+        newCopies[copies.length] = newCopy;
+        copies = newCopies;
     }
 
 
